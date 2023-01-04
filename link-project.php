6e7f5372b74550dab26ffd48f0b77094eda6aff8
Ekin Tertemiz <ekin.tertemiz@swisstph.ch>
2023-01-04 13:20:32 +0100

    imrpove log view

--- link-project.php
+++ link-project.php
@@ -10,12 +10,43 @@ $parameters = ['Tracking Action'];
 
 $result = $module->queryLogs($pseudoSql, $parameters);
 $logs = [];
-while($row = $result->fetch_assoc()){
+while($row = $result->fetch_object()){
     $logs[] = $row;
 }
+?>
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">Log Id</th>
+            <th scope="col">Date</th>
+            <th scope="col">Message</th>
+            <th scope="col">Action</th>
+            <th scope="col">User</th>
+            <th scope="col">Field</th>
+            <th scope="col">Owner</th>
+            <th scope="col">Session Id</th>
+        </tr>
+    </thead>
+  <tbody>
+  <?php
+foreach ($logs as $key => $log) {
 
-dump($logs);
+    echo '<tr scope="row">';
+    echo "<td>".$log->log_id."</td>";
+    echo "<td></td>";
+    echo "<td>".$log->message."</td>";
+    echo "<td>".$log->action."</td>";
+    echo "<td>".$log->user."</td>";
+    echo "<td>".$log->field."</td>";
+    echo "<td>".$log->owner."</td>";
+    echo "<td>".$log->instance."</td>";
+    echo "</tr>";
+}
 
+?>
+  </tbody>
+</table>
+<?php
 // $fieldName = "device_type";
 // $pid = 15;
 // $choiceLabels = $module->getChoiceLabels($fieldName, $pid);
