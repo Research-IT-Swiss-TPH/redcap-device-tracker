421c4e084faedf12a81581886ace1a7c689f3c12
Ekin Tertemiz <ekin.tertemiz@swisstph.ch>
2023-05-31 15:34:40 +0200

    Remove everyting

--- webpack.mix.js
+++
@@ -1,7 +0,0 @@
-let mix = require('laravel-mix');
-
-mix
-//.sass('src/style.scss', 'dist')
-.js('src/tracker/appTracker.js', 'dist')
-.js('src/monitor/appMonitor.js', 'dist')
-.vue({ version: 2 });
